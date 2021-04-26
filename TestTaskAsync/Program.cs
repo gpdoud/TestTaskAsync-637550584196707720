5f8029d259e482118d9616ec25eb3adb1a149e56
Greg Doud <gpdoud@gmail.com>
2021-04-26 18:27:01 -0400

    Committed by GitUtil

---
+++ TestTaskAsync/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestTaskAsync {
+    class Program {
+
+        void Run() {
+            var t = Task.Run(() => CallApi());
+            t.Wait();
+        }
+        void CallApi() {
+            
+        }
+        static void Main(string[] args) {
+            (new Program()).Run();
+            Thread.Sleep(5000);
+        }
+    }
+}
